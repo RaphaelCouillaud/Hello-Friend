37319a4370115857ed96b6cf5b03862ace5dead4
RaphaelCouillaud <couillaud.raphael@gmail.com>
2021-03-21 01:44:42 +0100

    figure bio responsive

--- index.js
+++ index.js
@@ -269,7 +269,7 @@ const toTop = document.querySelector(".to-top");
 
 window.addEventListener("scroll", () => {
   
-  if (window.pageYOffset > 500) {
+  if (window.pageYOffset > 1000) {
     toTop.classList.add("active");
   } else {
     toTop.classList.remove("active");
